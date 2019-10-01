5da1de7fcb0afb72ee4fc5fb217b58f27b249126
daichi nb <nd940501@gmail.com>
2019-10-01 20:59:42 +0900

    新規投稿、一覧画面、ログイン機能作成

---
+++ association_tutorial/app/controllers/tweets_controller.rb
@@ -0,0 +1,32 @@
+class TweetsController < ApplicationController
+  # deviseのメソッドで「ログインしていないユーザーをログイン画面に送る」メソッド
+  before_action :authenticate_user!, except: [:index]
+  
+  def new
+    @tweet = Tweet.new
+  end
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def create
+    #  フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetに代入
+    @tweet = Tweet.new(tweet_params)
+    # deviseのメソッドを使って「ログインしている自分のid」を代入
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
+  end
+
+  private
+    def tweet_params
+        # tweetモデルのカラムのみを許可
+        params.require(:tweet).permit(:body)
+    end
+
+end
