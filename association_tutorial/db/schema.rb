b9c0d913c73dd4615fbbf8c43d9a1ecfb9487802
daichi nb <nd940501@gmail.com>
2019-10-03 21:48:49 +0900

    お気に入り機能追加

--- association_tutorial/db/schema.rb
+++ association_tutorial/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_111603) do
+ActiveRecord::Schema.define(version: 2019_10_03_105227) do
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "body"
